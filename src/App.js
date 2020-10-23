6aa18039425444013ec4ba74154feac0a0655920
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-10-23 13:18:26 -0400

    front end file skeleton

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World, how you been.....good to see you my old friend.</h1>
+    </div>
+  );
+}
+
+export default App;
