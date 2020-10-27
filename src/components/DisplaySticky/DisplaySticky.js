a27321df0af24d8105ab3841ba961b2fe7d1cb0e
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-27 18:16:33 -0400

    added the hover effect back on the done button

--- src/components/DisplaySticky/DisplaySticky.js
+++ src/components/DisplaySticky/DisplaySticky.js
@@ -4,7 +4,7 @@ import React from "react";
 import { motion } from "framer-motion";
 
 //Import Component CSS
-import './DisplaySticky.css';
+import "./DisplaySticky.css";
 
 // create framer motion variable for animating the done button
 const doneButtonVariant = {
@@ -21,61 +21,66 @@ const doneButtonVariant = {
 
 //Create DisplaySticky Component
 const DisplaySticky = (props) => {
-	let stickies = props.stickies;
+  let stickies = props.stickies;
 
-	console.log(stickies);
+  console.log(stickies);
 
-	return (
-		<>
-			{stickies && stickies.length > 0 ? (
-				<div id='sticky-display'>
-					<h2>To-Do</h2>
-					{stickies.map((sticky) => (
-						<div className='sticky'>
-							<section className='sticky-name'>Name: {sticky.task}</section>
-							<section className='sticky-complete-by'>
-								Complete By: {sticky.completeBy}
-							</section>
-							<section className='sticky-workSpace'>
-								Work Space: {sticky.workSpace}
-							</section>
-							<section className='sticky-description'>
-								Description: {sticky.description}
-							</section>
-							<button
-								className='sticky-set-done'
-								onClick={() => {
-									props.setDone(sticky);
-									props.history.push('/');
-								}}>
-								Done
-							</button>
-							<button
-								className='editStickyButton'
-								onClick={() => {
-									props.selectSticky(sticky);
-									props.history.push('/edit');
-									console.log(`edit button`, props.handleUpdate);
-								}}>
-								Edit
-							</button>
-							<div className='sticky-delete-and-add-person-container'>
-								<section
-									class='far fa-trash-alt'
-									onClick={() => {
-										props.deleteSticky(sticky);
-										props.history.push('/');
-									}}></section>
-								<section className='sticky-add-person'>+</section>
-							</div>
-						</div>
-					))}
-				</div>
-			) : (
-				<h3>Add Some Stickies!</h3>
-			)}
-		</>
-	);
+  return (
+    <>
+      {stickies && stickies.length > 0 ? (
+        <div id="sticky-display">
+          <h2>To-Do</h2>
+          {stickies.map((sticky) => (
+            <div className="sticky">
+              <section className="sticky-name">Name: {sticky.task}</section>
+              <section className="sticky-complete-by">
+                Complete By: {sticky.completeBy}
+              </section>
+              <section className="sticky-workSpace">
+                Work Space: {sticky.workSpace}
+              </section>
+              <section className="sticky-description">
+                Description: {sticky.description}
+              </section>
+              <motion.button
+                className="sticky-set-done"
+                variants={doneButtonVariant}
+                whileHover="whileHover"
+                onClick={() => {
+                  props.setDone(sticky);
+                  props.history.push("/");
+                }}
+              >
+                Done
+              </motion.button>
+              <button
+                className="editStickyButton"
+                onClick={() => {
+                  props.selectSticky(sticky);
+                  props.history.push("/edit");
+                  console.log(`edit button`, props.handleUpdate);
+                }}
+              >
+                Edit
+              </button>
+              <div className="sticky-delete-and-add-person-container">
+                <section
+                  class="far fa-trash-alt"
+                  onClick={() => {
+                    props.deleteSticky(sticky);
+                    props.history.push("/");
+                  }}
+                ></section>
+                <section className="sticky-add-person">+</section>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <h3>Add Some Stickies!</h3>
+      )}
+    </>
+  );
 };
 
 export default DisplaySticky;
