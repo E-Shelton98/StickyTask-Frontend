836df928a7d48a3da372bee33903d89826519220
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-29 11:51:42 -0400

    added animation to the tasker form

--- src/components/PersonForm/PersonForm.js
+++ src/components/PersonForm/PersonForm.js
@@ -1,47 +1,95 @@
-import React from 'react'
-import './PersonForm.css'
+import React from "react";
+import { motion } from "framer-motion";
+import "./PersonForm.css";
+
+// framer motion function for the person form
+const personFormVariant = {
+  animate: {
+    rotateX: 360,
+    scale: 1,
+    x: 0,
+    y: 50,
+  },
+};
+
+// create framer motion variable for animating the add tasker button
+const addTaskerButtonVariant = {
+  whileHover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      yoyo: Infinity,
+      duration: 0.4,
+    },
+  },
+  whileTap: { scale: 0.9 },
+};
 
 const PersonForm = (props) => {
-    console.log('person form',props)
+  console.log("person form", props);
 
-    const emptyPerson = {
-        name: '',
-        tasks:[]
-    }
+  const emptyPerson = {
+    name: "",
+    tasks: [],
+  };
 
-    //state for data form
-    const [formData, setFormData] = React.useState(emptyPerson)
+  //state for data form
+  const [formData, setFormData] = React.useState(emptyPerson);
 
-    const handleChange = (event) => {
-        setFormData({ ...formData, [event.target.name]: event.target.value });
-    }
-    //handleCreate Function for creating people
-    const handleCreate = (newPerson) => {
-		console.log(newPerson);
-		fetch(props.url + '/person/', {
-			method: 'post',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(newPerson),
-		});
-    };
-    
-      //handleSubmit function to list data to App
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+  //handleCreate Function for creating people
+  const handleCreate = (newPerson) => {
+    console.log(newPerson);
+    fetch(props.url + "/person/", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPerson),
+    });
+  };
+
+  //handleSubmit function to list data to App
   const handleSubmit = (event) => {
     console.log("this is formData: ", formData);
     handleCreate(formData);
     props.history.push("/");
   };
 
-    return (
-        <form onSubmit={() => {handleSubmit()}}>
-            <input type="text" id="person-name" name="name" value={formData.name} onChange={handleChange} />
+  return (
+    <motion.form
+      drag
+      variants={personFormVariant}
+      animate="animate"
+      transition={{
+        duration: 0.8,
+        ease: `easeOut`,
+      }}
+      onSubmit={() => {
+        handleSubmit();
+      }}
+    >
+      <input
+        type="text"
+        id="person-name"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+      />
 
-            <input id="personButton" type='submit' value='Add Tasker!'/>
-        </form>
+      <motion.input
+        variants={addTaskerButtonVariant}
+        whileHover="whileHover"
+        whileTap="whileTap"
+        id="personButton"
+        type="submit"
+        value="Add Tasker!"
+      />
+    </motion.form>
+  );
+};
 
-    )
-}
-
-export default PersonForm
+export default PersonForm;
