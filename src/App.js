f33566af42f87e60d1f1f4e0b890bcc377f9d0c5
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-29 23:41:58 -0400

    creating the team page

--- src/App.js
+++ src/App.js
@@ -2,25 +2,34 @@
 import React from "react";
 // import Framer Motion
 import { motion } from "framer-motion";
+// import Route and Link
+import { Route, Link } from "react-router-dom";
 //Import App CSS
 import "./App.css";
 //Import Sticky Form
 import Whiteboard from "./components/Whiteboard/Whiteboard";
+import TeamPage from "./components/TeamPage/TeamPage";
 
 function App() {
   return (
     <div className="App">
-      
-        <motion.h1
-          className="heading"
-          initial={{ y: -100 }}
-          animate={{ y: 0 }}
-          transition={{ duration: 1.5, type: `spring`, stiffness: 120 }}
-        >
-          Sticky Task
-        </motion.h1>
-      
+      <motion.h1
+        className="heading"
+        initial={{ y: -100 }}
+        animate={{ y: 0 }}
+        transition={{ duration: 1.5, type: `spring`, stiffness: 120 }}
+      >
+        Sticky Task
+      </motion.h1>
+
       <Whiteboard />
+      <div className="team-page">
+        <Link to="/teampage">
+          <button id="team-button">Meet The Team</button>
+        </Link>
+
+        <Route exact path="/teampage" render={(rp) => <TeamPage {...rp} />} />
+      </div>
     </div>
   );
 }
---
+++ src/components/TeamPage/TeamPage.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./TeamPage.css";
+
+const TeamPage = (props) => {
+  return (
+    <div className="team-page">
+      <h2 className="meet-the-team">Let Us Reintroduce Ourselves</h2>
+      <h3>Sean</h3>
+      <img
+        className="sean-image"
+        src="https://i.imgur.com/yjxYvHN.jpg"
+        alt=""
+      ></img>
+      <p className="sean-blurb">
+        Worked along with the magnificent minds of Frieda and Erik
+        <br /> to create a task manager app called Sticky Task.
+      </p>
+      <h3>Erik</h3>
+      <p></p>
+      <h3>Frieda</h3>
+      <p></p>
+    </div>
+  );
+};
+
+export default TeamPage;
