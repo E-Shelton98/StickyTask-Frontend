a6ba5ce1bf6edb8389e7f4662316472461587faf
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-30 00:55:18 -0400

    added Frieda's blurb

--- src/components/TeamPage/TeamPage.js
+++ src/components/TeamPage/TeamPage.js
@@ -14,18 +14,27 @@ const TeamPage = (props) => {
       </h2>
       <h3>Sean</h3>
       <motion.img
+        initial={{ x: 2000 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 200, type: "spring", stiffness: 120 }}
         whileHover={{ scale: 1.4 }}
         className="sean-image"
         src="https://i.imgur.com/yjxYvHN.jpg"
         alt=""
       ></motion.img>
       <p className="sean-blurb">
-        Worked along side the magnificent minds of Frieda and Erik
-        <br /> to create a task manager app called Sticky Task.
+        A New York City based software engineer who <br />
+        worked along side the magnificent minds of fellow software engineers,{" "}
+        <br />
+        Frieda and Erik, to fully develop the backend and front end <br /> of a
+        task manager app called "Sticky Task."
       </p>
       <h3>Erik</h3>
       <motion.img
         whileHover={{ scale: 1.4 }}
+        initial={{ x: -2000 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 200, type: "spring", stiffness: 120 }}
         className="erik-image"
         src="https://i.imgur.com/wYAXjm8t.jpg"
         alt=""
@@ -33,12 +42,23 @@ const TeamPage = (props) => {
       <p className="erik-blurb"></p>
       <h3>Frieda</h3>
       <motion.img
+        initial={{ x: 2000 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 200, type: "spring", stiffness: 120 }}
         whileHover={{ scale: 1.4 }}
         className="frieda-image"
         src="https://i.imgur.com/QQMZEkVt.jpg"
         alt=""
       ></motion.img>
-      <p className="frieda-blurb"></p>
+      <p className="frieda-blurb">
+        An up-state New Yorker turned down-state New Yorker, <br />
+        Frieda is a student at GA enjoying the process of slowly going bald from
+        pulling her hair out because of code. She has joined forces with Erik
+        and Sean to provide the world with a brand-new, state-of-the-art task
+        manager, Sticky Task! <br />
+        Frieda lent her expertise in design and functionality to help complete
+        this magnificent app before you.
+      </p>
     </div>
   );
 };
