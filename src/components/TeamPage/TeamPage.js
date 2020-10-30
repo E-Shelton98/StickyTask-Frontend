f8a07239fdd086b258fd1951e68e5fefa19a797c
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-30 00:19:43 -0400

    updated images, blurbs, and added animation

--- src/components/TeamPage/TeamPage.js
+++ src/components/TeamPage/TeamPage.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { motion } from "framer-motion";
+
 import "./TeamPage.css";
 
 const TeamPage = (props) => {
@@ -7,19 +9,32 @@ const TeamPage = (props) => {
     <div className="team-page">
       <h2 className="meet-the-team">Let Us Reintroduce Ourselves</h2>
       <h3>Sean</h3>
-      <img
+      <motion.img
+        whileHover={{ scale: 1.4 }}
         className="sean-image"
         src="https://i.imgur.com/yjxYvHN.jpg"
         alt=""
-      ></img>
+      ></motion.img>
       <p className="sean-blurb">
         Worked along side the magnificent minds of Frieda and Erik
         <br /> to create a task manager app called Sticky Task.
       </p>
       <h3>Erik</h3>
-      <p></p>
+      <motion.img
+        whileHover={{ scale: 1.4 }}
+        className="erik-image"
+        src="https://i.imgur.com/wYAXjm8t.jpg"
+        alt=""
+      ></motion.img>
+      <p className="erik-blurb"></p>
       <h3>Frieda</h3>
-      <p></p>
+      <motion.img
+        whileHover={{ scale: 1.4 }}
+        className="frieda-image"
+        src="https://i.imgur.com/QQMZEkVt.jpg"
+        alt=""
+      ></motion.img>
+      <p className="frieda-blurb"></p>
     </div>
   );
 };
